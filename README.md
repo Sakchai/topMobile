89fbbde0cda59ab0b17c8a85e1db677191ce0fe6
Sakchai Thanasamut <sakchai.t@qnet.net>
2023-07-02 13:13:48 +0700

    initial topMobile

---
+++ README.md
@@ -0,0 +1,19 @@
+# nopSuite Mobile Application
+
+![daily-task-manager](https://docs.nopadvance.com/lib/nopSuite%20Mobile%20Application%20400x400.png)
+
+**nopSuite nopCommerce Mobile application** the most complete & ready-to-use eCommerce mobile application made with love in India for your nopCommerce store. The application is a perfect blend of Architecture, Engineering & Flutter. This application covers all major nopCommerce functionalities to offer a smooth mobile commerce experience to your customers and also available with source code.
+
+
+
+The application uses our 100% free to use NopSuite front-end API for nopCommerce to connect with your nopCommerce website. However, the paid source code, expert support and development customization support is available for both the NopSuite front-end API and NopSuite nopCommerce application.
+
+
+
+**Supported nopCommerce version(s):** 4.40, 4.50.
+
+
+
+**Note:** This document supports and refers to the latest released version of the **nopSuite nopCommerce Mobile application.**
+
+Copyright © 2021 by NopAdvance LLP. All Rights Reserved.
---
+++ lib/Utils/SharedPreferences.dart
@@ -0,0 +1,26 @@
+/// This is a free for use and open source, most complete mobile application for nopCommerce platform based on Flutter and NopSuite front-end API for nopCommerce by NopAdvance LLP.
+/// Copyright (C) 2022 - NopAdvance LLP
+
+/// This program is free software: you can redistribute and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License.
+
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+/// This program is forever free software: you can modify and/or redistribute. One cannot sell or relicense the source code it under the terms of the NopAdvance Public license terms.
+
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class SharedPreferencesValues {
+  //static const String SHARED_PREFERENCE_TOKEN = "UserToken";
+  static const String SHARED_PREFERENCE_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOiIxNjg4Mjc1MTA3IiwiZXhwIjoiMTcxOTgxMTEwNyIsIkN1c3RvbWVySWQiOiIxIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiJiYzMyMzkyYS0yYzY1LTRiMzctOTBjYy0xM2NiMmJiNzRhODkiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJhZG1pbkB5b3VyU3RvcmUuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImFkbWluQHlvdXJTdG9yZS5jb20ifQ.yH2NbjQTluzYMF8oc4qFYpKX1qhtqwXXIIO6_ZaJQA8";
+  static const String SHARED_PREFERENCE_CURRENT_VERSION = "CurrentVersion";
+  static const String SHARED_PREFERENCE_REFRESH_TOKEN = "UserRefreshToken";
+ // static const String SHARED_PREFERENCE_IS_LOGIN = "IsUserLogin";
+  static const String SHARED_PREFERENCE_IS_LOGIN = "False";
+//  static const String SHARED_PREFERENCE_IS_WITHOUT_LOGIN = "IsWithoutLogin";
+  static const String SHARED_PREFERENCE_IS_WITHOUT_LOGIN = "True";
+//  static const String SHARED_PREFERENCE_IS_LOAD_LOCAL_DATA = "IsLoadLocalData";
+  static const String SHARED_PREFERENCE_IS_LOAD_LOCAL_DATA = "True";
+  static const String SHARED_PREFERENCE_LANGUAGE_ID = "1";
+ // static const String SHARED_PREFERENCE_LANGUAGE_ID = "LanguageID";
+  static const String SHARED_PREFERENCE_TOKEN_EXPIRATION_DATE = "TokenExpirationDate";
+}
